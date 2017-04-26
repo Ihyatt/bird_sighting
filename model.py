9e47d05c5498fc65be4f067df5dd2848a266e7f9
Inas Hyatt <inas.raheema@gmail.com>
2017-04-26 16:17:53 -0700

    update secret

--- model.py
+++ model.py
@@ -36,8 +36,6 @@ if __name__ == "__main__":
 
     from server import app
     connect_to_db(app, os.environ.get("DATABASE_URL"))
-
-    connect_to_db(app)
     print "Connected to DB."
     db.create_all()
 
