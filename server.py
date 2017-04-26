8a04e3a3e2446c128905c47a2a11bc224f1c9097
Inas Hyatt <inas.raheema@gmail.com>
2017-04-25 17:20:34 -0700

    prob for bird done

--- server.py
+++ server.py
@@ -19,25 +19,6 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def main_page():
 
-	# now = datetime.datetime.now()
-	# now_hour = datetime.time(now.hour)
-	# current_hour = str(now_hour)[:-3]
-	sightings = Sighting.query.all()
-	# birds = {}
-	# birds_time = {}
-
-	# for sight in sightings:
-	# 	if birds.get(sight.bird) == None:
-	# 		birds[sight.bird] = sight.quantity
-	# 	else: 
-	# 		birds[sight.bird] += sight.quantity
-
-	# for sight in sightings: 
-	# 	if birds_time.get(sight.bird) == None:
-	# 		birds_time[sight.bird] = 0 
-
-	# 	if current_hour == sight.time:
-	# 		birds_time[sight.bird] += sight.quantity
 
 	return render_template("index.html")
 
@@ -45,6 +26,7 @@ def main_page():
 def add_sighting():
 
 	bird = request.form.get("bird").lower()
+	print bird
 	quantity = request.form.get("quantity")
 
 	now = datetime.datetime.now()
@@ -57,6 +39,71 @@ def add_sighting():
 
 
 	return "sighting submitted"
+
+
+
+@app.route("/return-search.json", methods=['GET'])
+def view_sightings():
+
+	bird = request.args.get("bird")
+	probability = {}
+	sightings = Sighting.query.filter(Sighting.bird == bird).all()
+	most_time_sightings = 0
+	times = {}
+	print sightings
+
+	for sight in sightings:
+		if probability.get(sight.time) == None:
+			probability[sight.time] = sight.quantity
+		else:
+			probability[sight.time] += sight.quantity
+
+
+	for sight in probability:
+		if probability[sight] > most_time_sightings:
+			most_time_sightings = probability[sight]
+
+	for sight in probability:
+		if probability[sight] == most_time_sightings:
+			times[sight] = bird
+
+
+	return jsonify(times)
+
+
+
+
+
+@app.route("/return-all-birds", methods=['GET'])
+def view_all_birds():
+	sightings = Sighting.query.all()
+
+	now = datetime.datetime.now()
+	now_hour = datetime.time(now.hour)
+	current_hour = str(now_hour)[:-3]
+	birds = {}
+	birds_time = {}
+	birds_prob = {}
+
+	for sight in sightings:
+		if birds.get(sight.bird) == None:
+			birds[sight.bird] = sight.quantity
+		else: 
+			birds[sight.bird] += sight.quantity
+
+	for sight in sightings: 
+		if birds_time.get(sight.bird) == None:
+			birds_time[sight.bird] = 0 
+
+		if current_hour == sight.time:
+			birds_time[sight.bird] += sight.quantity
+
+	for bird in birds:
+		birds_prob[bird] = int((float(birds_time[bird]) / float(birds[bird])) * 100)
+
+	return jsonify(birds_prob)
+
+
 
 	
 
